1938d0b079377b99f0fdb8071eb949fe2fec76f3
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-08-23 15:28:12 +0300

    added ultimate-hooks. Separated service to its own hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,37 +1,5 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
+import { useEffect } from 'react'
+import { useField, useResource } from './hooks'
 
 const App = () => {
   const content = useField('text')
@@ -41,29 +9,38 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
+  useEffect(() => {
+    noteService.getAll(notes);
+    personService.getAll(persons);
+    // eslint-disable-next-line
+  }, [])
+
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    content.reset('')
   }
  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
+    name.reset('')
+    number.reset('')
   }
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input {...content.inputProps} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input {...name.inputProps} /> <br/>
+        number <input {...number.inputProps} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
