810cb9ae444b530a2ffb52c4e06c2d27445502fb
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-10-05 14:26:23 +0300

    part8 GraphQL

---
+++ part8/backend-graphql/index.js
@@ -0,0 +1,176 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+];
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "The Demon ",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+];
+
+const typeDefs = `
+  type Query {
+		bookCount: Int!
+		authorCount: Int!
+		allBooks(author: String, genre: String): [Book]
+		allAuthors: [Author]
+  },
+	type Book {
+		title: String
+		published: Int
+		author: String
+		genres: [String]
+	},
+	type Author {
+		name: String
+		born: Int
+		bookCount: Int
+	},
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			published: Int!
+			genres: [String!]!
+		): Book,
+		editAuthor(
+			name: String!
+			setBornTo: Int
+		): Author
+	}
+`;
+
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author);
+      } else if (args.genres) {
+        return books.filter((book) => book.genres.includes(args.genres));
+      }
+      return books;
+    },
+    allAuthors: () => authors,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      // returns -1 if not found
+      const authorIndex = authors.findIndex(
+        (author) => author.name === args.author
+      );
+
+      books = books.concat(book);
+      if (authorIndex === -1) {
+        const newAuthor = {
+          name: args.author,
+          born: args.born,
+          bookCount: 1,
+          id: uuid(),
+        };
+        authors = authors.concat(newAuthor);
+      } else {
+        authors[authorIndex].bookCount =
+          (authors[authorIndex].bookCount || 0) + 1;
+      }
+      return book;
+    },
+    editAuthor: (root, args) => {
+      if (!args.setBornTo) {
+        return null;
+      }
+
+      const authorIndex = authors.findIndex(
+        (author) => author.name === args.name
+      );
+      authors[authorIndex].born = args.setBornTo;
+
+      return authors[authorIndex];
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
