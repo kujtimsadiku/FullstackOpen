c3afd99e0b086a71731c79e45303605eb6bf0ccd
kujtimsadiku <33124748+kujtimsadiku@users.noreply.github.com>
2023-11-02 16:25:16 +0200

    small thing not done yet

--- part8/frontend-graphql/src/App.js
+++ part8/frontend-graphql/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 // import { Route, Routes } from "react-router-dom";
 // import { authorService } from "./service/author";
 
@@ -13,14 +14,14 @@ const App = () => {
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <button onClick={() => setPage("login")}>login</button>
       </div>
 
       <Authors show={page === "authors"} />
 
       <Books show={page === "books"} />
 
-      <NewBook show={page === "add"} />
+      <LoginForm show={page === "login"} />
     </div>
   );
 };
---
+++ part8/frontend-graphql/src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NewBook from "./NewBook";
+
+const LoginForm = (props) => {
+  return (
+    <div>
+      <NewBook show={false} />
+    </div>
+  );
+};
+
+export default LoginForm;
