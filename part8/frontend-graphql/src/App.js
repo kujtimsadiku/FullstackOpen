d03cf2e5e6b40e396a87d013142f52b4c657618b
kujtimsadiku <33124748+kujtimsadiku@users.noreply.github.com>
2023-11-06 16:20:06 +0200

    Update app.js logout function will be added

--- part8/frontend-graphql/src/App.js
+++ part8/frontend-graphql/src/App.js
@@ -2,12 +2,20 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import Login from "./components/Login";
+import { useApolloClient } from "@apollo/client";
 // import { Route, Routes } from "react-router-dom";
 // import { authorService } from "./service/author";
 
 const App = () => {
   const [page, setPage] = useState("home");
   const [token, setToken] = useState("");
+  const client = useApolloClient();
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   return (
     <div>
