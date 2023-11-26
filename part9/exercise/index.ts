ce50e067e1697342b14604cb4ce945ccde1cabcd
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-11-26 19:07:10 +0200

    update index.ts

--- part9/exercise/index.ts
+++ part9/exercise/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/exercise", (_req, res) => {
+  res.status(200).send(`<h1>Exercise</h1>`);
+});
+
 app.post("/exercise", (req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { daily_exercises, target } = req.body;
@@ -15,7 +19,7 @@ app.post("/exercise", (req, res) => {
       (value: number) => typeof value !== "number"
     )
   ) {
-    res.status(400).send({ error: "malformatted parameters" });
+    res.status(400).json({ error: "malformatted parameters" });
   }
 
   const exercise = CalculateResult(
@@ -23,7 +27,7 @@ app.post("/exercise", (req, res) => {
     target as number
   );
 
-  res.status(200).send(exercise);
+  res.status(200).json(exercise);
 });
 
 const PORT = 3002;
