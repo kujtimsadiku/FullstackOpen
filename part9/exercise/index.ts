7386dc81120d8d0386019f6a8f99c5b4da766848
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-11-26 18:33:58 +0200

    update index.ts

--- part9/exercise/index.ts
+++ part9/exercise/index.ts
@@ -6,16 +6,22 @@ const app = express();
 app.use(express.json());
 
 app.post("/exercise", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { daily_exercises, target } = req.body;
 
   if (
     isNaN(Number(target)) ||
-    daily_exercises.some((value: number) => typeof value !== "number")
+    (daily_exercises as number[]).some(
+      (value: number) => typeof value !== "number"
+    )
   ) {
     res.status(400).send({ error: "malformatted parameters" });
   }
 
-  const exercise = CalculateResult(daily_exercises, target);
+  const exercise = CalculateResult(
+    daily_exercises as number[],
+    target as number
+  );
 
   res.status(200).send(exercise);
 });
